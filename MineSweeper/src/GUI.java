02bf0e93593a5b9bc055a9395c4a2cd89a712bba
1900391 <1900391@01-19715.pusd.dom>
2024-04-26 11:40:29 -0700

    gui initialized

---
+++ MineSweeper/src/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+
+public class GUI {
+	
+	JFrame frame;
+	JPanel textPanel;
+	
+	
+}
