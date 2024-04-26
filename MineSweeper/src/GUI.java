88bef8b4af1e4d41e10eb5a5248305cb3b69d709
1900391 <1900391@01-19715.pusd.dom>
2024-04-26 11:59:17 -0700

    cellGUI initialized

--- MineSweeper/src/GUI.java
+++ MineSweeper/src/GUI.java
@@ -6,9 +6,18 @@ import java.util.*;
 
 
 public class GUI {
-	
 	JFrame frame;
 	JPanel textPanel;
+	JPanel buttonPanel;
+	JButton[][] buttons;
+	JLabel textField;
 	
-	
+// 	TODO: generate map of size A x A as specified by player
+//	public static void main(String[] args) {
+//		for (int rows = 0; rows < size; rows++) {
+//			for (int cols = 0; cols < size; cols++) {
+//				//generate guiCells;
+//			}
+//		}
+//	}
 }
