776816c0059620a67695bfd93a3889ef85d79af1
allanhotpotato <107225970+allanhotpotato@users.noreply.github.com>
2024-04-29 11:15:24 -0700

    cell class: boolean for revealed cells

--- MineSweeper/src/Cell.java
+++ MineSweeper/src/Cell.java
@@ -2,11 +2,29 @@
 public class Cell {
 	
 	private int value;
-	
-	public Cell(int v) {
+	private boolean unrevealed;
+	//value = -1: bomb 
+	//value != -1: number of mines around the cell
+	//unrevealed: true = unrevealed cell
+	//unrevealed: false = revealed cell
+	public Cell(int v, boolean r) {
 		value = v;
+		unrevealed = r;
 	}
+	
 	public int getValue() {
 		return value;
+	}
+	
+	public void setValue(int v) {
+		value = v;
+	}
+	
+	public boolean getReveal() {
+		return unrevealed;
+	}
+	
+	public void setReveal(boolean r) {
+		unrevealed = r;
 	}
 }
