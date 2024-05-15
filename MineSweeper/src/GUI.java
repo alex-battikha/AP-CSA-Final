f4390af5461e84b3a6665f339cc12b3801504ea6
1900391 <1900391@01-19715.pusd.dom>
2024-05-15 12:15:52 -0700

    5/15 progress on GUI gen

--- MineSweeper/src/GUI.java
+++ MineSweeper/src/GUI.java
@@ -3,19 +3,105 @@ import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
-public class GUI {
+
+public class GUI implements ActionListener {
 	JFrame frame;
 	JPanel textPanel;
 	JPanel buttonPanel;
 	JButton[][] buttons;
 	JLabel textField;
+	JComboBox c1;
+	
+	static int size = 0;
 	
 // 	TODO: generate map of size A x A as specified by player
-//	public static void main(String[] args) {
-//		for (int rows = 0; rows < size; rows++) {
-//			for (int cols = 0; cols < size; cols++) {
-//				//generate guiCells;
-//			}
-//		}
-//	}
+	
+	
+	public void main(String[] args) {
+		frame = new JFrame("MineSweeper");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(1000, 1000);
+		frame.setVisible(true);
+		frame.setLayout(null);
+		frame.setResizable(true);
+			
+		dropdownGen();
+	}
+	
+	public void dropdownGen() {
+		String s1[] = {"Easy", "Medium","Hard"};
+		
+		c1 = new JComboBox(s1);
+	    c1.setBounds(15,15,75,35);
+	    c1.setVisible(true);
+		frame.add(c1);
+		frame.setVisible(true);
+		
+		c1.addActionListener(new ActionListener() {  
+			public void actionPerformed(ActionEvent e) {       
+				//System.out.println(c1.getItemAt(c1.getSelectedIndex()));  
+				mapDifficulty(c1.getItemAt(c1.getSelectedIndex()));
+			}
+		});
+	}
+
+	
+	public void mapGenerator(int s) {
+		buttonPanel = new JPanel();
+		buttonPanel.setVisible(true);
+		buttonPanel.setLayout(new GridLayout(s, s));
+		
+		buttons = new JButton[s][s];
+		
+		System.out.println("Generation Size: " + s);
+		
+		//fix since resize() is depracated
+		if(s == 8) {
+			frame.resize(600, 600);
+		}
+		else if(s == 14) {
+			frame.resize(800, 800);
+		}
+		else if(s == 20) {
+			frame.resize(1000, 1000);
+		}
+		
+		for (int row = 0; row < buttons.length; row++) {
+			for (int col = 0; col < buttons[0].length; col++) {
+				buttons[row][col] = new JButton();
+				buttons[row][col].setFocusable(false);
+				buttons[row][col].addActionListener(this);
+				buttonPanel.add(buttons[row][col]);
+			}
+		}
+		
+		frame.add(buttonPanel);
+		
+		frame.revalidate();
+		frame.setLocationRelativeTo(null);
+				
+	}
+	
+	public void mapDifficulty(Object object) {
+		if (object.equals("Easy")) {
+			size = 8;
+		}
+		else if(object.equals("Medium")) {
+			size = 14;
+		}
+		else if(object.equals("Hard")) {
+			size = 20;
+		}
+		
+		mapGenerator(size);
+		
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	
 }
