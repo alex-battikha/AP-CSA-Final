3ae3273ffa799c2728043ce7887014b5e7e070b6
1909315 <1909315@01-19953.pusd.dom>
2024-05-17 11:36:53 -0700

    grrrr

--- MineSweeper/src/CellGUI.java
+++ MineSweeper/src/CellGUI.java
@@ -1,17 +1,56 @@
+import java.awt.Color;
 import java.awt.Graphics;
 
-public class CellGUI extends Cell{
+public class CellGUI{
 	public int x;
 	public int y;
+	public int value;
+	private enum State {
+		UNKOWN,
+		REVEAL,
+		FLAG,
+	}
+	State state = State.UNKOWN;
 	
-	public CellGUI(int xi, int yi, int v, boolean r, boolean f) {
-		super(v, r, f);
+	public CellGUI(int xi, int yi, int v) {
 		x = xi;
 		y = yi;
+		value = v;
+	}
+	
+	public void reveal() {
+		state = State.REVEAL;
+	}
+	
+	public void flag() {
+		state = State.FLAG;
+	}
+	
+	public void hide() {
+		state = State.UNKOWN;
 	}
 	
 	public void paint(Graphics g) {
-		
+		switch (state) {
+		g.setColor(Color.green);
+		g.drawRect(x, y, 50, 50);
+		case UNKOWN:
+			break;
+		case REVEAL:
+			g.setColor(Color.gray);
+			g.drawRect(x, y, 50, 50);
+			if (value == -1) {
+				g.setColor(Color.black);
+				g.drawOval(x+25, y+25, 10, 10);
+			}
+			else {
+				g.drawString("" + value, x, y);
+			}
+			break;
+		case FLAG:
+			g.drawRect(x, y, 50, 50);
+			//todo draw flag
+		}
 	}
 	
 }
