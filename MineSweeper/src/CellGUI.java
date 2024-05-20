620b6020b96c3d29e59eb89c295a6e64ef8a2edd
1909315 <1909315@01-19953.pusd.dom>
2024-05-20 11:19:16 -0700

    YESSSSS :)))))

--- MineSweeper/src/CellGUI.java
+++ MineSweeper/src/CellGUI.java
@@ -1,57 +1,35 @@
 import java.awt.Color;
 import java.awt.Graphics;
-
 public class CellGUI{
 	public int x;
 	public int y;
 	public int value;
-	private enum State {
-		UNKNOWN,
-		REVEAL,
-		FLAG,
-	}
-	State state = State.UNKNOWN;
+	Graphics g;
 	
-	public CellGUI(int xi, int yi, int v) {
+	public CellGUI(int xi, int yi, int v, Graphics g) {
 		x = xi;
 		y = yi;
 		value = v;
+		this.g = g;
 	}
 	
 	public void reveal() {
-		state = State.REVEAL;
-	}
-	
-	public void flag() {
-		state = State.FLAG;
-	}
-	
-	public void hide() {
-		state = State.UNKNOWN;
-	}
-	
-	public void paint(Graphics g) {
-
-		g.setColor(Color.green);
+		g.setColor(Color.gray);
 		g.drawRect(x, y, 50, 50);
-		switch (state) {
-		case UNKNOWN:
-			break;
-		case REVEAL:
-			g.setColor(Color.gray);
-			g.drawRect(x, y, 50, 50);
-			if (value == -1) {
-				g.setColor(Color.black);
-				g.drawOval(x+25, y+25, 10, 10);
-			}
-			else {
-				g.drawString("" + value, x, y);
-			}
-			break;
-		case FLAG:
-			g.drawRect(x, y, 50, 50);
-			//todo draw flag
+		if (value == -1) {
+			g.setColor(Color.black);
+			g.drawOval(x+25, y+25, 10, 10);
 		}
 	}
 	
+	public void flag() {
+		g.drawRect(x, y, 50, 50);
+		//todo draw flag
+	}
+	
+	public void hide() {
+		g.setColor(Color.green);
+		g.drawRect(x, y, 50, 50);
+	}
 }
+
